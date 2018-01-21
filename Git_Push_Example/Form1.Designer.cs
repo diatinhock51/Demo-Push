5ad9e758f8b53f09585257867c8bf5960e50d6ce
TlqV <rerangst@gmail.com>
2018-01-21 23:05:16 +0700

    Add project files.

---
+++ Git_Push_Example/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Git_Push_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbQuesion = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbQuesion
+            // 
+            this.lbQuesion.AutoSize = true;
+            this.lbQuesion.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQuesion.Location = new System.Drawing.Point(56, 26);
+            this.lbQuesion.Name = "lbQuesion";
+            this.lbQuesion.Size = new System.Drawing.Size(187, 23);
+            this.lbQuesion.TabIndex = 0;
+            this.lbQuesion.Text = "DO U LOVE ME???";
+            // 
+            // btnYes
+            // 
+            this.btnYes.FlatAppearance.BorderSize = 0;
+            this.btnYes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYes.Location = new System.Drawing.Point(12, 63);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(75, 23);
+            this.btnYes.TabIndex = 1;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.FlatAppearance.BorderSize = 0;
+            this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNo.Location = new System.Drawing.Point(195, 63);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 100);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lbQuesion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbQuesion;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
+
---
+++ Git_Push_Example/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Git_Push_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            if (btnNo.Text == "Yes")
+            {
+                MessageBox.Show("I know that!!!");
+                this.Close();
+            }
+            if (btnNo.Text == "No")
+            {
+                btnNo.Text = "Yes";
+                btnYes.Text = "No";
+            }               
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            if (btnYes.Text == "Yes")
+            {
+                MessageBox.Show("I know that!!!");
+                this.Close();
+            }
+            if (btnYes.Text == "No")
+            {                
+                btnYes.Text = "Yes";
+                btnNo.Text = "No";
+            }
+        }
+    }
+}
